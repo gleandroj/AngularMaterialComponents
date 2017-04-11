6eaaa820f14077745006e0ab5d94cc223750c8b2
Gabriel <gabrielleandrojunior@live.com>
2017-04-11 13:44:29 -0300

    change module name

--- src/js/services/index.services.js
+++ src/js/services/index.services.js
@@ -6,7 +6,7 @@ import AngularUtilService from './AngularUtilService';
 import ImageHelperService from './ImageHelperService';
 import CameraService from './CameraService';
 
-let services = angular.module('angular-material-components.services', ['ngMaterial']);
+let services = angular.module('angular-material-simple-components.services', ['ngMaterial']);
 
 services.service('AngularUtilService', AngularUtilService);
 services.service('ImageHelperService', ImageHelperService);
